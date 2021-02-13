be1c61f1d85342a3819a745968380e5159bdc1ad
mccrarymatt <mccrarymatt@byui.edu>
2021-02-13 12:58:11 -0500

    tweak windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -5,7 +5,7 @@ let windSpeed = parseFloat(document.getElementById("windSpeed").innerHTML)
 if (temp <= 50 && windSpeed > 3.0) {
 
     let windChill = 35.74 + 0.6215 * temp - 35.75 * windSpeed ** 0.16 + 0.4275 * temp * windSpeed ** 0.16;
-    document.getElementById("windChill").innerHTML = parseInt(windChill)
+    document.getElementById("windChill").innerHTML = windChill.toFixed(2)
 
 } else {
 
